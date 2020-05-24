e9e1beab7f14263cd4bbeeb4dcf9d727ffee965e
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-24 09:47:39 -0400

    private dns vnet link by terraform

--- dns-zone.tf
+++ dns-zone.tf
@@ -1,13 +1,15 @@
-resource "null_resource" "dns_zone_link" {
+data "external" "get_aks_private_dns_zone_name" {
+  program = ["/bin/sh", "${path.module}/get-aks-private-dns-zone-name.sh"]
 
-  provisioner "local-exec" {
-    interpreter = ["bash"]
-    command = "dns-zone-link.sh"
-
-    environment = {
-      DNS_VNET = "${var.custom_dns_vnet_id}"
-      AKS_RESOURCE_GROUP="${var.resource_group_name}"
-      AKS_CLUSTER_NAME="${var.cluster_name}"
-    }
+  query = {
+    aksNodesResourceGroup = azurerm_kubernetes_cluster.aks.node_resource_group
   }
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_vnet_link_aks_jb" {
+  name                  = local.jb_name
+  resource_group_name   = azurerm_kubernetes_cluster.aks.node_resource_group
+  private_dns_zone_name = data.external.get_aks_private_dns_zone_name.result.aks_private_dns_zone
+  virtual_network_id    = var.custom_dns_vnet_id
+  registration_enabled  = false
 }
