47bcd3f5c75668cf5c8555a41bfd661cc2d7c1c5
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-24 09:50:26 -0400

    Update dns-zone.tf

--- dns-zone.tf
+++ dns-zone.tf
@@ -7,7 +7,7 @@ data "external" "get_aks_private_dns_zone_name" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_vnet_link_aks_jb" {
-  name                  = local.jb_name
+  name                  = "private-dns-link"
   resource_group_name   = azurerm_kubernetes_cluster.aks.node_resource_group
   private_dns_zone_name = data.external.get_aks_private_dns_zone_name.result.aks_private_dns_zone
   virtual_network_id    = var.custom_dns_vnet_id
